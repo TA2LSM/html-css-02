c850d534dca5cf198c49124832e95fb0e8a045cc
TA2LSM <semihsenol@gmail.com>
2022-04-03 18:23:07 +0300

    first commit

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@
 const switcher = document.querySelector(".btnThemeSelect");
 
 // "switcher"a, tıklandığı zamanı yakalayan bir "event listener" tanımlandı
+// Tema değiştiren kısım burası
 switcher.addEventListener("click", function () {
   var className = document.body.className;
   if (className === "dark-theme") {
@@ -19,3 +20,21 @@ switcher.addEventListener("click", function () {
   className = document.body.className;
   console.log("Current class name: " + className);
 });
+
+// Örnek 8'deki resme tıklandığında çalışacak kısım burası
+function imgMappingFunc(data) {
+  switch (data) {
+    case 1:
+      alert("Bilgisayar'a tıkladınız.");
+      break;
+    case 2:
+      alert("Telefon'a tıkladınız.");
+      break;
+    case 3:
+      alert("Kahve'ye tıkladınız.");
+      break;
+    default:
+      alert("Geçersiz seçim!");
+      break;
+  }
+}
